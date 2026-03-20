a4afbee344b30e57a7a5875bed67e49592080154
helloYash112 <rajputyashwardhan17747@gmail.com>
2026-03-21 00:45:08 +0530

    application property

--- src/main/java/com/swastika_company/PowerReading/PowerReadingApplication.java
+++ src/main/java/com/swastika_company/PowerReading/PowerReadingApplication.java
@@ -1,31 +1,14 @@
 package com.swastika_company.PowerReading;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.swastika_company.PowerReading.entity.Meter;
-import com.swastika_company.PowerReading.entity.MeterReading;
-import com.swastika_company.PowerReading.entity.User;
-import com.swastika_company.PowerReading.repository.UserRepo;
-
 
 @SpringBootApplication
 public class PowerReadingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PowerReadingApplication.class, args);
-		
-		
+
 	}
-	
-	
 
 }
